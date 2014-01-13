a054fe93d657adca7e8fae905b0f04ac029744c9
Matthew Thebert <matt@thebert.com>
2014-01-12 22:03:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+AngulAir Sample Angular App
+===========================
+
+Ingredients:
+- Angular 1.1.5
+- Bootstrap 2.3.2
---
+++ app/js/controllers/app.js
@@ -0,0 +1,41 @@
+function AppCtrl ($scope) {
+  $scope.airports = {
+    "PDX": {
+      "code": "PDX",
+      "name": "Portland International Airport",
+      "city": "Portland",
+      "destinations": [
+        "LAX",
+        "SFO"
+      ]
+    },
+    "STL": {
+      "code": "STL",
+      "name": "Lambert-St. Louis International Airport",
+      "city": "St. Louis",
+      "destinations": [
+        "LAX",
+        "MKE"
+      ]
+    },
+    "MCI": {
+      "code": "MCI",
+      "name": "Kansas City International Airport",
+      "city": "Kansas City",
+      "destinations": [
+        "LAX",
+        "DFW"
+      ]
+    }
+  };
+  $scope.sidebarURL = "partials/airport.html"
+  $scope.editAirportURL = "partials/editAirport.html"
+  $scope.editAirport = null;
+  $scope.currentAirport = null;
+  $scope.setAirport = function (code){
+    $scope.currentAirport = $scope.airports[code];
+  };
+  $scope.setEditAirport = function (code){
+    $scope.editAirport = $scope.airports[code];
+  };  
+}
